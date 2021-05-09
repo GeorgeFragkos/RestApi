92b44b2c7f17fa923cabf55b15d7b5a546d0d457
George Frgakos <83181849+GeorgeFragkos@users.noreply.github.com>
2021-05-09 23:13:56 +0300

    Adding controller to tesr api key auth

---
+++ API/Controllers/V1/TestApiKeyController.cs
@@ -0,0 +1,15 @@
+﻿using API.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.V1
+{
+    [ApiKeyAuth]
+    public class TestApiKeyController : ControllerBase
+    {
+        [HttpGet("message")]
+        public IActionResult GetMessage()
+        {
+            return Ok("ApiKey works :)");
+        }
+    }
+}
