21f36459fefd8067d55aad2e84548cde637f65c9
George Frgakos <83181849+GeorgeFragkos@users.noreply.github.com>
2021-05-09 23:13:29 +0300

    Adding filter for ApiKey auth

---
+++ API/Filters/ApiKeyAuthAttribute.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class ApiKeyAuthAttribute : Attribute, IAsyncActionFilter
+    {
+        private const string _ApiKeyHeaderName = "ApiKey";
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.HttpContext.Request.Headers.TryGetValue(_ApiKeyHeaderName, out var potentialApiKey))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            var configuration = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+            var apiKey = configuration.GetValue<string>("ApiKey");
+
+            if (!apiKey.Equals(potentialApiKey))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+            await next();
+        }
+    }
+}
